cb156bfb29d04dd7ea0852af29bbe1bfd839419e
jade0829 <bibe0829@gmail.com>
2022-07-12 16:08:01 +0900

    gluesys CRD

---
+++ api/v1/glsstorage_types.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// GlsstorageSpec defines the desired state of Glsstorage
+type SC struct {
+	stype          string `json:"stype,omitempty"`
+	gluster_host   string `json:"gluster-host,omitempty"`
+	gluster_volume string `json:"gluster-volume,omitempty"`
+	nvmetype       string `json:"nvme-type,omitempty"`
+	nvme_host      string `json:"nvme-host,omitempty"`
+	nvme_fs        string `json:"nvme-fs,omitempty"`
+}
+
+type PVC struct {
+	accessMode string `json:"accessmode,omitempty"`
+	limit      string `json:"limit,omitempty"`
+}
+type GlsstorageSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Glsstorage. Edit glsstorage_types.go to remove/update
+	name string `json:"name,omitempty"`
+	sc   SC     `json:"sc,omitempty"`
+	pvc  PVC    `json:"pvc,omitempty"`
+}
+
+// GlsstorageStatus defines the observed state of Glsstorage
+type GlsstorageStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Glsstorage is the Schema for the glsstorages API
+type Glsstorage struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GlsstorageSpec   `json:"spec,omitempty"`
+	Status GlsstorageStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// GlsstorageList contains a list of Glsstorage
+type GlsstorageList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Glsstorage `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Glsstorage{}, &GlsstorageList{})
+}
---
+++ controllers/glsstorage_controller.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	glsv1 "github.com/jade0829/crd/api/v1"
+)
+
+// GlsstorageReconciler reconciles a Glsstorage object
+type GlsstorageReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=gls.excrd.com,resources=glsstorages,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=gls.excrd.com,resources=glsstorages/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=gls.excrd.com,resources=glsstorages/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Glsstorage object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
+func (r *GlsstorageReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	Glsstorage = &glsv1.Glsstorage{}
+
+	err := r.Client.Get(ctx, req.NamespacedName, Glsstorage)
+
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	scConfig := Glsstorage.sc
+	pvcConfig := Glsstorage.pvc
+
+	var provisioner string
+
+	if scConfig.stype == "gluster" {
+		provisioner = "kadalu"
+	} else if scConfig.stype == "nvme" {
+		provisioner = "csi.gluesys.io"
+	}
+	GlsSC := &storagev1.StorageClass{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: Glsstorage.name + "sc",
+		},
+		Provisioner: provisioner,
+	}
+
+	_ = r.Client.Create(context.Backgroud(), GlsSC)
+
+	GlsPVC := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: Glsstorage.name,
+		},
+		Spec: &corev1.PersistentVolumeClaimSpec{
+			AccesccModes: []corev1.PersistentVolumeAccessMode{
+				"ReadWriteMany",
+			},
+			storageClassName: Glsstorage.name + "sc",
+			Resources: corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: pvcConfig.limit,
+				},
+			},
+		},
+	}
+
+	_ = r.Client.Create(context.Backgroud(), GlsPVC)
+
+	return ctrl.Result{}, nil
+}
+
+func (r *GlsstorageReconciler) Delete(pvcConfig pvcConfig) error {
+	ctx := context.Background()
+
+	pvcClient := &corev1.PersistentVolumeClaim.Delete()
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *GlsstorageReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&glsv1.Glsstorage{}).
+		Complete(r)
+}
